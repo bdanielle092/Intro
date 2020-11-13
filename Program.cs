687c478dfb6ea2a8a99c581e8edcb2288dd4b669
Faith Magras <bdanielle092@gmail.com>
2020-11-13 10:48:30 -0600

    added comments

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// using System is the same as an import in javascript 
+using System;
+//namespace is naming the module
+namespace Intro
+{
+    class Program
+    {
+        //Line 9 is a method which is just like funcation in javascript 
+        //(string[] args) is a parameter. string is the type and args is the name of the parameter and the [] is saying it will be an array
+        //void means there is no return  so main will not be able to return anything s
+        static void Main(string[] args)
+        {
+            //Console.WriteLine is console.log in javascript
+            //("Hello World!") is a string 
+            //the code on line 15 changes the background color in the terminal 
+            // Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// the csproj is just like package json file. Package dependency
+
+//let name; is declearing a variable
+//name = "Adam" is assigning a variable
+// = sign is the assignment variable 
+// const cohort = 43; first declearing a variable then assigning the variable
+//const person = {name: "Adam", cohort: 43}; is an object
+//const languages = ["JS", "C#"] is an array of strings
+//this is a function and has one parameter and is returning a string 
+// const sayHello = (name) => {
+//     return `Hello, ${name}`
+//     }
+
